6dca843d51b0652670a1a6c9b30532a6efc86a40
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-06 12:51:32 +0100

    w

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Domain.Activity> Activities { get; set; }
+    }
+}
