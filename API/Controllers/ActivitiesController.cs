55db443f0c6c3a24dbf310ece5e3b4e011f6ce71
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-08 19:26:57 +0100

    w

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -17,13 +17,21 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new .Query());
+            return await Mediator.Send(new ActivityList.Query());
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query{Id=id});
         }
-        
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity});
+
+            return Ok();
+        }
     }
 }
---
+++ Application/Activities/Create.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Domain.Activity Activity { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext Context)
+            {
+                _context = Context;
+            }
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                await _context.Activities.AddAsync(request.Activity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
