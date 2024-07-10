62dc60873c13d312f123eb4272b507a74742f689
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-10 18:36:25 +0100

    request updated

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { Container, Header, List } from "semantic-ui-react";
+import { Container} from "semantic-ui-react";
 import { Activity } from "./models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
+import LoadingComponents from "./LoadingComponents";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity|undefined>(undefined)
   const [editMode, setEditMode] = useState(false);
+  const [Loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get<Activity[]>("http://localhost:5000/api/activities").then((response) => {
-      setActivities(response.data);
+    agent.Activities.list().then((response) => {
+      let activities: Activity[] = [];
+      response.forEach((activity) => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     });
   }, []);
 
@@ -42,6 +50,8 @@ function App() {
   function handleDeleteActivity(id: string){
     setActivities([...activities.filter(x=> x.id !==id)])
   }
+
+  if (Loading) return <LoadingComponents  content="Loading app"/>
   return (
     <>
      <NavBar openForm={handleFormOpen} />
