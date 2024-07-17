de1a316d67a72b0ed8d7a6ca53d178e6e0f4da6e
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-17 01:40:48 +0100

    Adding styling to UI

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,31 +1,32 @@
-import {useEffect} from 'react'
-import { Grid, GridColumn,  } from 'semantic-ui-react'
+import { useEffect } from 'react'
+import { Grid, GridColumn, } from 'semantic-ui-react'
 import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import LoadingComponents from '../../../app/layout/LoadingComponents';
+import ActivityFilters from './ActivityFilters';
 
 
 export default observer(function ActivityDashboard() {
 
-    const {activityStore} = useStore();
-    const {loadActivities, activityRegistry}= activityStore;
+  const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
 
   useEffect(() => {
-    if(activityRegistry.size <= 1) loadActivities();
+    if (activityRegistry.size <= 1) loadActivities();
   }, [loadActivities, activityRegistry.size]);
 
 
-  if (activityStore.loadingInitial) return <LoadingComponents  content="Loading app"/>
-    
+  if (activityStore.loadingInitial) return <LoadingComponents content="Loading app" />
+
   return (
     <Grid>
-        <Grid.Column width='10'>
-            <ActivityList />
-        </Grid.Column>
-        <GridColumn width='6'>
-          <h2>Activity filters</h2>
-        </GridColumn>
+      <Grid.Column width='10'>
+        <ActivityList />
+      </Grid.Column>
+      <GridColumn width='6'>
+        <ActivityFilters />
+      </GridColumn>
     </Grid>
   )
 });
