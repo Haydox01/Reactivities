185e0f2106715282194c4d9acc4cd08dc99e9f5d
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-21 10:39:34 +0100

    cotd on error handling

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using MediatR;
 using Application.Core;
-using Application;
 
 namespace API.Controllers
 {
