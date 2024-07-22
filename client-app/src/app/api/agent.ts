65cb0214e72cdf382fffd0e4765d6c4b4e368ed6
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-22 20:50:28 +0100

    Working on form

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -15,21 +15,25 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 },(error: AxiosError)=> {
-    const {data, status}= error.response as AxiosResponse;  
+    const {data, status, config}= error.response as AxiosResponse;  
     switch (status) {
         case 400:
-           if(data.errors){
-            const modalStateErrors =[];
-            for(const key in data.errors){
-                if (data.errors[key]){
-                    modalStateErrors.push(data.errors[key])
-                }
-            }
-            throw modalStateErrors.flat();
-        }
-        else{
-            toast.error(data);
-        }
+            router.navigate('/server-error');
+            // if(config.method === 'get' && data.errors.hasOwnProperty('id')){
+            //     router.navigate('/not-found');
+            // }
+        //    if(data.errors){
+        //     const modalStateErrors =[];
+        //     for(const key in data.errors){
+        //         if (data.errors[key]){
+        //             modalStateErrors.push(data.errors[key])
+        //         }
+        //     }
+        //     throw modalStateErrors.flat();
+        // }
+        // else{
+        //     toast.error(data);
+        // }
             break;
         case 401:
             toast.error('unauthorized')
@@ -39,6 +43,7 @@ axios.interceptors.response.use(async response => {
             break;
         case 500:
             store.commonStore.setServerError(data);
+            router.navigate('/server-error');
             break;
         case 403:
             toast.error('forbidden')
