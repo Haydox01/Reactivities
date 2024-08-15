961c4ea9f4d6981c2a8140b1eb2cdab13cc5a08d
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-08-15 09:41:24 +0100

    Image upload impl

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -2,11 +2,6 @@
 using Application.Profiles;
 using AutoMapper;
 using Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Core
 {
@@ -18,10 +13,13 @@ namespace Application.Core
             CreateMap<Domain.Activity, ActivityDto>().ForMember(d => d.HostUserName, o=> o.MapFrom(s=> s.Attendees
             .FirstOrDefault(x=> x.IsHost).AppUser.UserName));
 
-            CreateMap<ActivityAttendee, Profiles.AttendeesProfile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.UserName, o => o.MapFrom(s => s.AppUser.UserName))
-                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<AppUser, AttendeesProfile>()
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
                 
         }
     }
---
+++ Application/Profiles/Details.cs
@@ -0,0 +1,45 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class Details
+    {
+        public class Query : IRequest<Result<AttendeesProfile>>
+        {
+            public string UserName { get; set; }
+        }
+        public class Handler : IRequestHandler<Query, Result<AttendeesProfile>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task<Result<AttendeesProfile>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.ProjectTo<AttendeesProfile>(_mapper.ConfigurationProvider)
+                    .SingleOrDefaultAsync(x => x.UserName == request.UserName);
+
+                if (user == null) return null;
+
+                return Result<AttendeesProfile>.Success(user);
+            }
+
+       
+        }
+    }
+}
