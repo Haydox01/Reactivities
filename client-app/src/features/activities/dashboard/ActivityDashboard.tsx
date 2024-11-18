3066432aa692a44a4116677d242e7f7e64331991
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-11-18 15:13:08 +0100

    Removed api keys

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,24 @@
-import { useEffect } from 'react'
-import { Grid, GridColumn, } from 'semantic-ui-react'
+import { useEffect, useState } from 'react'
+import { Button, Grid, GridColumn, } from 'semantic-ui-react'
 import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import LoadingComponents from '../../../app/layout/LoadingComponents';
 import ActivityFilters from './ActivityFilters';
+import { PagingParams } from '../../../app/layout/models/pagination';
 
 
 export default observer(function ActivityDashboard() {
 
   const { activityStore } = useStore();
-  const { loadActivities, activityRegistry } = activityStore;
+  const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore;
+  const[loadingNext, setLoadingNext] = useState(false);
+
+  function handleGetNext(){
+    setLoadingNext(true);
+    setPagingParams(new PagingParams(pagination!.currentPage + 1))
+    loadActivities().then(() => setLoadingNext(false));
+  }
 
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
@@ -23,6 +31,14 @@ export default observer(function ActivityDashboard() {
     <Grid>
       <Grid.Column width='10'>
         <ActivityList />
+        <Button
+        floated='right'
+        content='More...'
+        positive
+        onClick={handleGetNext}
+        loading={loadingNext}
+        disabled={pagination?.totalPages === pagination?.currentPage} 
+        />
       </Grid.Column>
       <GridColumn width='6'>
         <ActivityFilters />
